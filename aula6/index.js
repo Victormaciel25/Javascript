e1c51cb08e978e111b7fcd3501fa873a36710996
Victor Maciel <victormaciel.dev@gmail.com>
2024-02-26 19:52:59 -0300

    Update index.js

--- aula6/index.js
+++ aula6/index.js
@@ -1,8 +1,35 @@
-let nome = 'Otávio'; //String
+let nome; //Declarou a variável
+nome = 'Victor'; //Inicializando a variável
+console.log(nome);
+nome = 'Maciel';
+console.log(nome);
 
+// Não podemos criar variáveis com palavras reservadas
+// Ex:
+// let if;
 
-console.log(nome,'nasceu em 1984.');
-console.log('Em 2000',nome,'conheceu Maria.');
-console.log('João casou-se com Maria em 2012.');
-console.log('Maria teve 1 filho com João em 2015.');
-console.log('O filho de João se chama Eduardo.');
+// Variáveis precisam ter nomes significativos
+// Ex:
+let nomeCliente1 = 'João';
+console.log(nomeCliente1);
+
+// Não pode começar o nome de uma variável com número
+// let 1nome;
+
+// Utilizamos camelCase(primeira palavra iniciada com letra minuscula e as proximas juntas com inicio em letra maiuscula)
+// Ex:
+let nomeCompletoDoCliente = 'Victor Maciel';
+console.log(nomeCompletoDoCliente);
+
+// Case-sensitive
+// Ex:
+let nomeCliente = 'Victor';
+let nomecliente = 'Maciel';
+console.log(nomeCliente, nomecliente);
+
+// Não podemos redeclarar variáveis com let
+let cliente = 'Victor';
+cliente = 'Maciel';
+console.log(cliente);
+
+// Não utilize VAR, utilize LET.
