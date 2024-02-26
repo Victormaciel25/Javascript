4a15eb06f7f0727fdeb775820991ede46df475d3
Victor Maciel <victormaciel.dev@gmail.com>
2024-02-26 19:13:28 -0300

    up

---
+++ aula6/index.js
@@ -0,0 +1,8 @@
+let nome = 'Otávio'; //String
+
+
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000',nome,'conheceu Maria.');
+console.log('João casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com João em 2015.');
+console.log('O filho de João se chama Eduardo.');
