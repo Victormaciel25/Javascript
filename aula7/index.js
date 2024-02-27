0331a2709ef80457843c77154b0c389f97a9d07e
Victor Maciel <victormaciel.dev@gmail.com>
2024-02-27 01:45:32 -0300

    Create index.js

---
+++ aula7/index.js
@@ -0,0 +1,23 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+const nome = 'João';
+console.log(nome);  
+
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
+console.log(typeof(resultadoTriplicado));
+console.log(typeof(primeiroNumero + segundoNumero));
