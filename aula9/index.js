fcb4c126076084a99639ea5f2a2d948db0b39b58
Victor Maciel <victormaciel.dev@gmail.com>
2024-02-28 13:21:51 -0300

    Create index.js

---
+++ aula9/index.js
@@ -0,0 +1,6 @@
+// ECMAScript 2015 (ES6)
+
+var nome = 'Luiz';
+var nome = 'Otávio'; // É possivel redeclara uma variável usando VAR, porém não é possivel usando LET.(Sempre usar const ou let)
+
+console.log(nome);
