de1a5b10a5b44c53cb4a1d9db3cd060d462cf107
Victor Maciel <victormaciel.dev@gmail.com>
2024-02-29 11:54:43 -0300

    Create index.js

---
+++ aula11/index.js
@@ -0,0 +1,30 @@
+/* 
+    Aritméticos + - / *
+    + Adição / concatenação
+    ** Potenciação
+    % Resto da divisão
+
+    Incremento = ++
+    Decremento = --
+
+*/
+let contador = 1;
+contador++;
+contador++;
+contador++;
+contador++;
+
+console.log(contador);
+console.log(++contador);
+console.log(--contador);
+
+/* 
+    NaN - Not a number 
+    parseInt(converta para um numero inteiro)
+    parseFloat(converte para um numero decimal)
+*/
+
+const num1 = 10;
+const num2 = parseFloat('50,3')
+console.log(num1 + num2);
+console.log(typeof num2);
