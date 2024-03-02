bce0d2f3eff1067f1efa0c38012ae24b664ae700
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-02 02:08:07 -0300

    up

--- aula12/js/script.js
+++ aula12/js/script.js
@@ -2,5 +2,5 @@ const num1 = parseInt(prompt('Digite um número:'));
 const num2 = parseInt(prompt('Digite um número:'));
 
 const resultado = num1 + num2;
-alert('O resultado foi ' + resultado);
+alert('O resultado foi: ' + resultado);
 
---
+++ aula14/script.js
@@ -0,0 +1,22 @@
+//               01234567
+let umaString = 'Um texto';
+
+console.log(umaString.charAt(4)); // Usa para selecionar uma casa da variável
+
+console.log(umaString.concat(' em', ' um', ' lindo dia.')); // Usa para juntar algo com uma variável
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`);
+
+console.log(umaString.indexOf('o', 3)); // Usa para encontrar o indice da letra selecionada, da direção esquerda para direita.
+console.log(umaString.lastIndexOf('m', 3)); // Usa para encontrar o indice da letra selecionada, da direção direita para esquerda.
+
+let umaString2 = 'O rato roeu a roupa do rei de roma.';
+
+console.log(umaString2.replace(/r/g, '#')); // Usa para trocar uma letra por outra selecionada.
+
+console.log(umaString2.length); // Usa para saber o tamanho da string.
+console.log(umaString2.slice(2, 6)); // Usa para separar alguma palavra da string.
+console.log(umaString2.slice(-5, -1)); // É possivel pegar de trás para frente utilizando o numero negativo.
+
+
+
