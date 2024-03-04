e571706fd97e238bd19a56684e653979934acee6
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-04 00:29:22 -0300

    Update index.js

--- aula15/index.js
+++ aula15/index.js
@@ -1,5 +1,15 @@
-let num1 = 1500;
-let num2 = 2.5;
+// IEEE 754-2008
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2 // 0.8
+num1 += num2 // 0.9 
+num1 += num2 // 1.0
+
+num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1));
 
 // console.log(num1.toString() + num2);
 // num1 = num1.toString();
