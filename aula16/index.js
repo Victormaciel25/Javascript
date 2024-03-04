820820e45ae119fe563b9696b98fbb5b2fb4cd10
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-04 15:30:28 -0300

    Create index.js

---
+++ aula16/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.49;
+// let num2 = Math.floor(num1); //Arredonda o numero para baixo
+// let num2 = Math.ceil(num1); //Arredonda o numero para cima
+// let num2 = Math.round(num1); // Arredonda para o numero mais proximo
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Pega o maior numero da sequência
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Pega o menor numero da sequência
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+           // Math.random - mostra numero aleatórios
+           // Math.round - arredonda o numero
+console.log(aleatorio);
