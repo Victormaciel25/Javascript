8145f7181a4326777f6e1feb1afff5b823e0ac34
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-03 23:43:42 -0300

    Update index.js

--- aula15/index.js
+++ aula15/index.js
@@ -1,5 +1,10 @@
-let num1 = 1;
+let num1 = 1500;
 let num2 = 2.5;
 
-console.log(num1.toString() + num2);
-console.log(typeof num1);
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+console.log(num1.toString(2)); // O '2' após o toString é usado para transformar o numero em binário.
+console.log(num1.toFixed(2)); // O '2' após o toFixed é usado para botar 2 casas decimais antes do ponto.
+console.log(Number.isInteger(num1)); // Verifica se o numero é inteiro(True), se não(false).
+let temp = num1 * '5';
+console.log(Number.isNaN(temp)); // 
