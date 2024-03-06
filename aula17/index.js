9922867e9562d00b8060b43cc1c3721645aa3361
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-06 01:08:18 -0300

    Create index.js

---
+++ aula17/index.js
@@ -0,0 +1,28 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, 3)); // slice - é usado para fatiar
+
+// delete alunos[1]; // delete - exclui o elemento indicado
+
+// alunos.shift(); // shift - remove no começo
+// alunos.pop(); // pop - remove no fim
+
+// alunos.unshift('Fábio'); //unshift - adiciona no começo
+// alunos.unshift('Luiza');
+
+// alunos.push('Luiza'); //push - adiciona no fim do array
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza';   // [alunos.length] - adiciona no fim do array
+// alunos[alunos.length] = 'Luana';
+// alunos[alunos.length] = 'Fábio';
+
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Luiza';
+
