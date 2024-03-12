e1de8e626b955873d48044e669aa8d04e14b4c85
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-12 17:34:07 -0300

    Create index.js

---
+++ aula26/index.js
@@ -0,0 +1,11 @@
+const numero = 10;
+
+// Se (numero >= 0 && numero <=5) ocorrer, faça isso {código}
+// Se não faça isso{o código}
+
+if (numero >= 0 && numero <=5) {
+    console.log('O número está entre 0 e 5.');
+} else {
+    console.log('O número NÃO está entre 0 e 5.');
+}
+
