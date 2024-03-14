14040b984dcc3644933bc05ef4771b014062bd55
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-13 23:03:18 -0300

    Update main.js

--- aula27/assets/js/main.js
+++ aula27/assets/js/main.js
@@ -5,17 +5,19 @@ const form = document.querySelector('#form');
 // Formula para enviar o formulario e previnir a pagina
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log('Evento previnido.');
-    setResultado('Olá mundo!');
+    const inputPeso = e.target.querySelector('#peso');
+    console.log(inputPeso);
 });
+
+function criaP (className) {
+    const p = document.createElement('p'); // Criando um paragrafo na const p.
+    return p;
+}
 
 function setResultado (msg) {
     const resultado = document.querySelector('#resultado'); // Criando const e vinculando á div com id #resultado.
     resultado.innerHTML = ''; // Enviando para a div uma string sem nada.
-    const p = document.createElement('p'); // Criando um paragrafo na const p.
-    p.classList.add('paragrafo-resultado'); // Adicionando uma class ao paragrafo criado.
-    p.innerHTML = 'Qualquer coisa'; // Enviando uma string dentro de um paragrafo para a div.
     resultado.appendChild(p); // Incluindo a const p dentro da const resultado que se refere a div #resultado.
-
-
+    const p = criaP();
+    p.
 }
