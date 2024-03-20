9a2eebc6bf7d0e60d2aceae6d72392ad61472ce2
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-20 01:28:25 -0300

    Create index.js

---
+++ aula32/index.js
@@ -0,0 +1,17 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco {...bloco}
+// Var só tem escopo de função
+
+let nome = 'Luiz'; // criando
+var nome2 = 'Luiz';
+
+if (verdadeira) {
+    let nome = 'Otávio'; // criando
+    console.log(nome, nome2);
+
+    if (verdadeira){
+        let nome = 'Outra coisa'
+        console.log(nome, nome2);
+    }
+}
