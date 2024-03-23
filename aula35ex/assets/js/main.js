0cb54d78ff015b7b5e4a166e6d64c26343e6fff4
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-23 01:15:44 -0300

    up

---
+++ aula35ex/assets/js/main.js
@@ -0,0 +1,13 @@
+const elementos = [
+    {tag: 'p', texto: 'Qualquer texto que você quiser.'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'section', texto: 'Frase 3'},
+    {tag: 'footer', texto: 'Frase 4'},
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+    console.log(elementos[i]);
+}
