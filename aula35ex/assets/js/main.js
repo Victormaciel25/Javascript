c8c96280c269baeb8a7e5abd43d934b97a42f8c0
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-25 08:25:45 -0300

    Update main.js

--- aula35ex/assets/js/main.js
+++ aula35ex/assets/js/main.js
@@ -9,10 +9,10 @@ const container = document.querySelector('.container');
 const div = document.createElement('div');
 
 for (let i = 0; i < elementos.length; i++) {
-    let {tag , texto} = elementos[i];
-    let tagCriada = document.createElement(tag);
-    tagCriada.innerText = texto;
-    div.appendChild(tagCriada);
+    let {tag , texto} = elementos[i]; // Pega os indices dos elementos tag e texto
+    let tagCriada = document.createElement(tag); // Cria tag 
+    tagCriada.innerText = texto; // Inseri o texto dentro da tag
+    div.appendChild(tagCriada); // Adiciona a tag dentro da div
 }
 
 container.appendChild(div);
