b60ed0000b1c6ac2cdcc39f404f65763cc2ac645
Victor Maciel <victormaciel.dev@gmail.com>
2024-03-24 23:09:41 -0300

    Update main.js

--- aula35ex/assets/js/main.js
+++ aula35ex/assets/js/main.js
@@ -9,5 +9,10 @@ const container = document.querySelector('.container');
 const div = document.createElement('div');
 
 for (let i = 0; i < elementos.length; i++) {
-    console.log(elementos[i]);
+    let {tag , texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
 }
+
+container.appendChild(div);
