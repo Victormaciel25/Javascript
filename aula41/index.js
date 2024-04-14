ee098ca93c0b349fdd73dc3d560963589cc71aa4
Victor Maciel <victormaciel.dev@gmail.com>
2024-04-14 01:19:40 -0300

    Create index.js

---
+++ aula41/index.js
@@ -0,0 +1,11 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function numeros(x, y) {
+    if (x > y) {
+        console.log(x);
+    } else {
+        console.log(y);
+    }
+}
+
+numeros(1, 4);
