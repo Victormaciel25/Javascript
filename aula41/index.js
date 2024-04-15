c4d0b45e85f7624f131730f8434d25e01ccf0376
Victor Maciel <victormaciel.dev@gmail.com>
2024-04-15 07:39:06 -0300

    Update index.js

--- aula41/index.js
+++ aula41/index.js
@@ -1,11 +1,35 @@
 // Escreva uma função que recebe 2 números e retorne o maior deles
 
 function numeros(x, y) {
-    if (x > y) {
-        console.log(x);
+    if (x > y) { 
+        console.log(x); 
     } else {
         console.log(y);
     }
 }
 
-numeros(1, 4);
+// ou
+
+function numeros(x, y) {
+    if (x > y) console.log(x); 
+    else console.log(y);
+}
+
+// ou
+
+function numeros(x, y) {
+    return x > y ? x : y;
+}
+
+// ou
+
+const numeros2 = (x, y) => {
+    return x > y ? x : y;
+};
+
+// simplificando o código acima
+
+const numeros3 = (x, y) => x > y ? x : y;
+
+//numeros2(1, 4);
+console.log(numeros2(1, 4));
