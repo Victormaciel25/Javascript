e881d6dd5edc6e18865aa04e6a4397e26aed2386
Victor Maciel <victormaciel.dev@gmail.com>
2024-04-17 23:46:02 -0300

    Update index.js

--- aula43/index.js
+++ aula43/index.js
@@ -33,3 +33,17 @@ function check(x) {
 
 check(x);
 
+// Explicação do professor
+
+function fizzBuzz(numero) {
+    if (typeof numero !== 'number') return numero;
+    if (numero % 3 ===0 && numero % 5 ===0) return 'FizzBuzz';
+    if (numero % 3 ===0) return 'Fizz';
+    if (numero % 5 ===0) return 'Buzz';
+    return numero;
+}
+
+console.log('a', fizzBuzz('a'))
+for (let i = 0; i <= 100; i++) {
+    console.log(i, fizzBuzz(i))
+}
