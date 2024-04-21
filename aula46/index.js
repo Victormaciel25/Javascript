aaa4f77bc01491f6ecb09c7de9c28da034ad3308
Victor Maciel <victormaciel.dev@gmail.com>
2024-04-21 00:02:48 -0300

    Update index.js

--- aula46/index.js
+++ aula46/index.js
@@ -6,4 +6,15 @@ function mostraHora() {
     });
 }
 
-console.log(mostraHora());
+const timer = setInterval(function() {
+    console.log(mostraHora());
+}, 1000);
+
+setTimeout(function() {
+    clearInterval(timer);
+}, 3000);
+
+setTimeout(function(){
+    console.log('Olá mundo!');
+}, 5000);
+
