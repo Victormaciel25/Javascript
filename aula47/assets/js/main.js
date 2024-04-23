e90d77698134828f0e845e5103565a8a2dd7c03d
Victor Maciel <victormaciel.dev@gmail.com>
2024-04-23 00:29:18 -0300

    up

---
+++ aula47/assets/js/main.js
@@ -0,0 +1,16 @@
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+iniciar.addEventListener('click', function(event){
+    relogio.innerHTML = 'Cliquei no iniciar';
+});
+
+pausar.addEventListener('click', function(event){
+    alert('Cliquei no pausar');
+});
+
+zerar.addEventListener('click', function(event){
+    alert('Cliquei no zerar');
+});
